bc3cdac9fbb30f81e2e7ea675de8c43e9bb5cfc0
teovlt <teo.villet2@gmail.com>
2025-10-28 13:44:21 +0100

    Rename main file

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,13 +1,11 @@
-package SpringForce.demo;
-
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class ProjectApplication {
+public class Main {
 
 	public static void main(String[] args) {
-		SpringApplication.run(ProjectApplication.class, args);
+		SpringApplication.run(Main.class, args);
 	}
 
 }
