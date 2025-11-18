d87831f826a0c48627916c1f0c557e9e8a27144f
teovlt <teo.villet2@gmail.com>
2025-11-18 15:29:33 +0100

    finish basic crud

--- src/main/java/fr/imt/springForce/vehicle/presentation/controller/VehicleController.java
+++ src/main/java/fr/imt/springForce/vehicle/presentation/controller/VehicleController.java
@@ -15,13 +15,28 @@ public class VehicleController {
     private final VehicleService vehicleService;
 
     @GetMapping("/")
-    public List<Vehicle> findAll() {
+    public List<Vehicle> findAllVehicles() {
         return vehicleService.findAll();
+    }
+
+    @GetMapping("/:id")
+    public Vehicle findVehicleById(@PathVariable String vehicleId) {
+        return vehicleService.findById(vehicleId);
     }
 
     @PostMapping("/")
     public Vehicle createVehicle(@RequestBody Vehicle vehicle) {
         return vehicleService.create(vehicle);
+    }
+
+    @PutMapping("/:id")
+    public Vehicle updateVehicle(@RequestBody Vehicle vehicle, @PathVariable String vehicleId) {
+        return vehicleService.update(vehicle, vehicleId);
+    }
+
+    @DeleteMapping("/:id")
+    public void deleteVehicle(@PathVariable String vehicleId) {
+         vehicleService.delete(vehicleId);
     }
 
 }
