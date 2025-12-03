1c6c29eab1f921cfbf3a67325969a20c6e005a1b
Arnaud ENDIGNOUS <BL204483@htm-group.com>
2025-12-03 15:50:55 +0100

    fix(): removed generate command from Customer

--- src/main/java/fr/imt/springforce/customer/infrastructure/persistence/CustomerDocument.java
+++ src/main/java/fr/imt/springforce/customer/infrastructure/persistence/CustomerDocument.java
@@ -30,6 +30,8 @@ public class CustomerDocument {
 
     private String licenceNumber;
 
+    private java.time.Instant birthDate;
+
     private Address address;
 
 }
--- src/main/java/fr/imt/springforce/customer/infrastructure/persistence/MongoCustomerRepository.java
+++ src/main/java/fr/imt/springforce/customer/infrastructure/persistence/MongoCustomerRepository.java
@@ -6,9 +6,19 @@ import java.util.Optional;
 import java.util.UUID;
 
 public interface MongoCustomerRepository extends MongoRepository<CustomerDocument, String> {
+
     Optional<CustomerDocument> findByPublicId(UUID publicId);
+
     boolean existsByPublicId(UUID publicId);
+
     void deleteByPublicId(UUID publicId);
+
     boolean existsByLicenceNumber(String licenceNumber);
+
     CustomerDocument findByEmail(String email);
+
+    boolean existsByFirstNameAndFamilyNameAndBirthDate(String firstName, String familyName, java.time.Instant birthDate);
+
+    boolean existsByEmail(String email);
+
 }
