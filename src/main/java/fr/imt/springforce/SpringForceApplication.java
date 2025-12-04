58d81cd6590b381d0a29b2b33cb8cc43d5867408
Arnaud ENDIGNOUS <BL204483@htm-group.com>
2025-12-04 16:25:18 +0100

    feat(): swagger support

--- src/main/java/fr/imt/springforce/SpringForceApplication.java
+++ src/main/java/fr/imt/springforce/SpringForceApplication.java
@@ -1,9 +1,12 @@
 package fr.imt.springforce;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@OpenAPIDefinition(info = @Info(title = "SpringForce API", version = "1.0", description = "Documentation SpringForce API v1.0"))
 public class SpringForceApplication {
 
     public static void main(String[] args) {
